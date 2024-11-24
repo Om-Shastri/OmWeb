4836b3a6f294e51d38d0ea6f4b3adb7040fa371d
Om-Shastri <omshastri@gmail.com>
2024-11-24 14:43:54 -0500

    Om

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Om Shastri",
   description: "Welcome to the personal website of Om Shastri, a tech enthusiast.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -26,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body style={{ backgroundColor: 'black' }}>
         <Spline
           scene="https://prod.spline.design/Fm2oH0q1qSQyzm0J/scene.splinecode"
