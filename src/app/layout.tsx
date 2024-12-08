1c279e7054fd62030bccf4374cf1e1a79f2a3439
Om-Shastri <omshastri@gmail.com>
2024-12-07 21:05:37 -0500

    om

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Spline from '@splinetool/react-spline/next';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -33,10 +33,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body style={{ backgroundColor: 'black' }}>
-        <Spline
-          scene="https://prod.spline.design/Fm2oH0q1qSQyzm0J/scene.splinecode"
-          className="fixed top-0 left-0 w-full h-full -z-20"
-        />
         {children}
       </body>
     </html>
